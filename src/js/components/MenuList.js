08cb818550e4e885cf15980dc5a7e4c5e0bf53e9
reyemgs <thetortyan@gmail.com>
2020-11-16 12:54:24 +0300

    minor changes MenuList

--- src/js/components/MenuList.js
+++ src/js/components/MenuList.js
@@ -1,7 +1,7 @@
 export default class MenuList {
     constructor(props) {
         this.sideBar = document.querySelector('#sidebar-wrapper');
-        this.items = props
+        this.items = props;
     }
 
     render() {
