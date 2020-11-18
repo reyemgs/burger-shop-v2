e11b7521031d73e1fa436cf707a99bccbd0fde37
reyemgs <thetortyan@gmail.com>
2020-11-18 12:22:20 +0300

    render modal method

--- src/js/components/Modal.js
+++ src/js/components/Modal.js
@@ -3,4 +3,65 @@ export default class Modal {
         this.navigationItems = props;
         this.eventHandler = handler;
     }
+
+    render() {
+        const wrapper = document.createElement('div');
+        wrapper.classList.add('modal-wrapper');
+
+        const content = document.createElement('div');
+        content.className = 'modal-content';
+
+        const closeButton = document.createElement('div');
+        closeButton.className = 'close-modal';
+        closeButton.innerHTML = '<i class="fas fa-times fa-2x"></i>';
+
+        const nextButton = document.createElement('button');
+        nextButton.className = 'next-button';
+        nextButton.innerHTML = 'ВПЕРЕД';
+
+        const previousButton = document.createElement('button');
+        previousButton.className = 'previous-button';
+        previousButton.innerHTML = 'НАЗАД';
+
+        const inBasketButton = document.createElement('button');
+        inBasketButton.className = 'modal-in-basket';
+        inBasketButton.innerHTML = 'В КОРЗИНУ';
+
+        const title = document.createElement('span');
+        title.className = 'modal-title';
+
+        const header = document.createElement('div');
+        header.className = 'modal-header';
+        header.append(title, closeButton);
+
+        const footer = document.createElement('div');
+        footer.className = 'modal-footer';
+
+        const price = document.createElement('span');
+        price.className = 'modal-price';
+        price.innerHTML = `Цена: `;
+
+        const ul = document.createElement('ul');
+        ul.className = 'items-list';
+
+        for (const item of this.menuItems) {
+            const li = document.createElement('li');
+
+            li.setAttribute('data-category', item.category);
+            li.className = 'modal-menu-item';
+            li.innerHTML = item.name;
+
+            ul.append(li);
+        }
+
+        const itemWrapper = document.createElement('div');
+        itemWrapper.className = 'items-wrapper';
+        itemWrapper.append(previousButton, ul, nextButton);
+
+        const shadow = document.createElement('div');
+        shadow.classList.add('shadow-modal');
+
+        wrapper.append(header, itemWrapper, content, footer);
+        document.body.append(wrapper, shadow);
+    }
 }
