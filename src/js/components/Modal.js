1f872d4b68fc654c6d2d3c1c27f2355ba6ec18ca
reyemgs <thetortyan@gmail.com>
2020-11-19 10:26:53 +0300

    emit render

--- src/js/components/Modal.js
+++ src/js/components/Modal.js
@@ -42,6 +42,7 @@ export default class Modal {
 
         const menuItem = this.getMenuItem(this.currentPage);
         this.activePage(menuItem);
+        this.eventHandler.emit('renderIngridientsByCategory', menuItem.category);
     }
 
     previousPage() {
@@ -50,6 +51,7 @@ export default class Modal {
 
         const menuItem = this.getMenuItem(this.currentPage);
         this.activePage(menuItem);
+        this.eventHandler.emit('renderIngridientsByCategory', menuItem.category);
     }
 
     activePage(menuItem) {
