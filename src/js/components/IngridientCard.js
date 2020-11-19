df705236d9c8b993f0084109c0bcf183d7c5c2f0
reyemgs <thetortyan@gmail.com>
2020-11-19 09:35:49 +0300

    style change

---
+++ src/js/components/IngridientCard.js
@@ -0,0 +1,11 @@
+export default class IngridientCard {
+    constructor(props) {
+        this.id = props.id;
+        this.key = props.key;
+        this.name = props.name;
+        this.price = props.price;
+        this.image = props.image;
+        this.description = props.description;
+        this.category = props.category;
+    }
+}
