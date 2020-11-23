47f1c7631bce083db234dd70ff958706de9c3c8f
reyemgs <thetortyan@gmail.com>
2020-11-23 10:06:06 +0300

    remove resetPrice when open

--- src/js/components/Modal.js
+++ src/js/components/Modal.js
@@ -34,7 +34,6 @@ export default class Modal {
 
         this.eventHandler.emit('renderIngridientsByCategory', menuItem.category);
         this.activateIngridients(menuItem.category);
-        this.calculatePrice();
         this.createPrice();
     }
 
